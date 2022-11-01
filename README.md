88b8048678fa74b668411aef6b8c84663d67315f
willbillm <mcginnis.william1@gmail.com>
2022-11-01 09:40:10 -0700

    readme update

---
+++ README.md
@@ -0,0 +1,3 @@
+This site is online at 'https://twooter.netlify.app'. 
+
+Visiting the site or opening the client end in dev mode will automatically access the server.
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import userRoutes from './routes/user.js'
 import postRoutes from './routes/post.js'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 
 const app = express();
@@ -18,7 +21,7 @@ app.use('/posts', postRoutes);
 app.get('/', (req, res) => {
     res.send('hello world')
 })
-const CONNECTION_URL = process.env.MONGODB_URI
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.${process.env.DB_STRING}.mongodb.net/?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {
